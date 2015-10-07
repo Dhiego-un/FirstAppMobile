6ed2a3538b3dd95f2505c6d381e5b8e008ead127
USER <USER@SAKE-PC>
2015-10-07 08:59:03 -0300

    Acabou

--- public_html/js/inicializacao.js
+++ public_html/js/inicializacao.js
@@ -13,3 +13,9 @@
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
+  
+  
+   $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
