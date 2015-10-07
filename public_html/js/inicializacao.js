0e4fa2573344851056082451431e1ef03cd75769
USER <USER@SAKE-PC>
2015-10-07 08:18:33 -0300

    Terminando o conteudo.

--- public_html/js/inicializacao.js
+++ public_html/js/inicializacao.js
@@ -7,3 +7,9 @@
  $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+    
+  $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
